37e7be92522ffa5120ba626537f1bc74570933d0
Jaime Pineda <jpin730@pm.me>
2024-03-07 18:47:14 -0600

    feat: :zap: add database connection

---
+++ src/main/java/org/example/utils/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package org.example.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/db";
+    private static final String USER = "user";
+    private static final String PASSWORD = "password";
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connected to the database");
+            } catch (SQLException e) {
+                System.out.println("An error occurred in database connection");
+                throw e;
+            }
+        }
+
+        return connection;
+    }
+}
